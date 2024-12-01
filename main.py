c4db70bdedcec71507d2aa62af784f037df635f2
ZaraZoha786 <mzarabha@gmail.com>
2024-12-01 14:51:54 +0200

    pass

--- main.py
+++ main.py
@@ -5,3 +5,19 @@ for i in a:
     break
   else:
     print ("A not found")
+
+for x in range(10):
+    if x % 20 == 0:
+       print("twist")
+
+    elif x % 15 == 0:
+       pass
+
+    elif x % 5 == 0:
+       print("fizz")
+
+    elif x % 3 == 0:
+       print("buzz")
+
+    else:
+       print(x)
